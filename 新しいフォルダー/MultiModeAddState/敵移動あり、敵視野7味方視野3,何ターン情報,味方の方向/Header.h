084139fbd617933bec5cd395464ad286cbeb3272
kenpos <kenpos13@gmail.com>
2017-01-10 12:26:24 +0900

    コミット

--- 新しいフォルダー/MultiModeAddState/敵移動あり、敵視野7味方視野3,何ターン情報,味方の方向/Header.h
+++ 新しいフォルダー/MultiModeAddState/敵移動あり、敵視野7味方視野3,何ターン情報,味方の方向/Header.h
@@ -1,6 +1,16 @@
+﻿//
+//  Header.h
+//  LeanForceLearning
+//
+//  Created by nakagawakenta on 12/29/16.
+//  Copyright © 2016 nakagawakenta. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
 #include <iostream>
 #include <utility>
-#include <direct.h>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -11,34 +21,38 @@
 
 //���ԋ���
 struct State {
-	int first;
-	int second;
-	int locate_enemy_count;
+  int first;
+  int second;
+  int pdirection;
+  int locate_enemy_count;
 };
 
 struct outputData {
-	int first;
-	int second;
-	int efirst;
-	int esecond;
+  int first;
+  int second;
+  int efirst;
+  int esecond;
 };
 
-const int eyesight = 7;//���E�̍L��
-const int qSize = eyesight * 2 + 1;//Q�l�̑傫��
+const int e_eysight = 3;//���E�̍L��
+const int qSize = e_eysight * 2 + 1;//Q�l�̑傫��
+const int p_esight = 7;
+//const int p_qSize = p_esight * 2 + 1;//Q�l�̑傫��
 const int mapsize = 15;
 const int ACTION = 5;
+const bool pldirection = true;
 const bool blindcount = true;//���^�[���G�����Ă��Ȃ����@�Ƃ������Ԑ��𔽉f�����邩�ۂ�
-const bool checkmovenemy = false;
-const int MAXGAME = 5000000;//��
-const int EPISODECOUNT = 500;
-const int EVALUATIONCOUNT = 10;
-const int EPSILON = 30;
+const bool checkmovenemy = true;
+const int MAXGAME = 100000000;//��
+const int EPISODECOUNT = 2000;
+const int EVALUATIONCOUNT = 10000;
+const int EPSILON = 40;
 
-const double gamma = 0.8;
+const double ganna = 0.8;
 const double alpha = 0.1;
 const double faild = 0;
 const double rewards = 100;
-const double subrewards = 1;
+const double subrewards = 0;
 
 //const std::string foldaname = "ganna" + std::to_string(gamma)
 //+ "alpha" + std::to_string(alpha)
@@ -55,6 +69,7 @@ const double subrewards = 1;
 //static bool checkExistenceOfFolder(const std::string folder_name);
 void resetmap();
 void drawMap();
+State initState(int, int);
 void setPlayer(State player);
 void setEnemy(State enemy);
 void resetPlayer(State player);
@@ -65,14 +80,21 @@ State checkCharacter(State character, int action);
 State protCharactor(State player, int action);
 void initializeQvalue();
 State searchRelationEnemy(State playerpositions, State enemypositons);
+int searchdirectionPlayer(State myposi, State player2);
+
 int getMaxQAction(State state, int playernum);
 int chooseAnAction(State playerstate, int playernum);
-//int QlearningMethod(State p1, State p2, State enemy, int gamecount);
-int SoloQlearningMethod(State p1, State enemy, int gamecount);
-bool calcSoloReward(State state, State afterstate, int action, State player, State enemy, long double AttenuationAlpha);
-//bool calcReward(State state, State afterstate, int action, State player, State player2, State enemy, int playernumber, long double AttenuationAlpha);
-State initState(int, int);
+int chooseEvalAnAction(State playerstate, int playernum);
+
+int MultiQlearningMethod(State p1, State p2, State enemy, int gamecount);
 void outputQvalueTable(int gamecount);
-int SoloQlearningEvaluationMethod(State p1, State enemy, int gamecount, int evacount);
+int MultiQlearningEvaluationMethod(State p1, State p2, State enemy, int gamecount, int evacount);
 void EvaluationFunction(int evacount);
-void outputEvaluationMoveData(int evacount, int gamecount, std::vector<outputData> d);
+
+int getMAXQValue(State afterstate, int playernumber);
+bool calcSuccessReward(State state, int action, double maxQ, long double AttenuationAlpha, int playernum);
+bool calcFinishReward(State state, int action, double maxQ, long double AttenuationAlpha, int playernu);
+bool calcFaildReward(State state, int action, double maxQ, long double AttenuationAlpha, int playernum);
+
+
+#endif /* Header_h */
